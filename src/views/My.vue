1f69b2c47b3ff8de322f8a7d8f9518d5da04d4d3
moring <3108824149@qq.com>
2021-08-29 20:42:32 +0800

    提交

--- src/views/My.vue
+++ src/views/My.vue
@@ -3,6 +3,27 @@
         <div class="my-top">
             <p class="my-top-img"><img src="/img/02.png" alt=""></p>
             <p class="my-top-p">登录/注册</p>
+            <div class="my-top-two">
+                <ul>
+                    <li>
+                        <p class="my-top-ul-li-p">0</p>
+                        <p>我的学习</p>
+                    </li>
+                    <li>
+                        <p class="my-top-ul-li-p1">0</p>
+                        <p>本周课时</p>
+                    </li>
+                    <li>
+                        <p class="my-top-ul-li-p1">0</p>
+                        <p>我的积分</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- 分 -->
+        <div class="my-fen"></div>
+        <div class="my-con">
+
         </div>
     </div>
 </template>
@@ -44,5 +65,30 @@ export default {
     font-size: 18px;
     color: #fff;
     margin-top: 10px;
+}
+.my-top-two{
+    width: 375px;
+    height: 74px;
+    margin-top: 90px;
+}
+.my-top-two ul{
+    list-style: none;
+    display: flex;
+}
+.my-top-two ul li{
+    width: 33%;
+}
+.my-top-two ul li p{
+    font-size: 15px;
+    margin-top: 5px;
+}
+.my-top-ul-li-p{
+    color: #5bb8f5;
+}
+.my-top-ul-li-p1{
+    color: #e60012;
+}
+.my-fen{
+    
 }
 </style>
