a33cb39f464f66fbfb0fe7c80ce803ea290e92a3
moring <3108824149@qq.com>
2021-08-31 08:49:32 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
     <router-view class="home-view"></router-view>
     <Tabbar></Tabbar>
-=======
-   <div class="box"></div>
-   <van-button type="primary">主要按钮</van-button>
-<van-button type="info">信息按钮</van-button>
-<van-button type="default">默认按钮</van-button>
-<van-button type="warning">警告按钮</van-button>
-<van-button type="danger">危险按钮</van-button>
->>>>>>> 5aa661143c30f490c8276e7f3ec2e22c353b20e2
   </div>
 </template>
 
@@ -21,7 +12,6 @@ export default {
   components:{Tabbar}
 }
 </script>
-<<<<<<< HEAD
 
 
 <style>
@@ -31,12 +21,5 @@ export default {
 }
 .home-view{
   overflow: auto;
-=======
-<style>
-.box{
-  width: 375px;
-  height: 100px;
-  background: red
->>>>>>> 5aa661143c30f490c8276e7f3ec2e22c353b20e2
 }
 </style>
